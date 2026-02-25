20aeda3eeb108776a46a2af43ad8d4aa372b9031
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-25 13:01:54 +0500

    Add Project No:1

--- Web-Dev-Practice/DOM.js
+++ Web-Dev-Practice/DOM.js
@@ -1,1 +1,9 @@
-
+//   Project NO : 1 BUTTON CLICK ON OFF.
+let button=document.querySelector("button");
+button.addEventListener("click",function(){
+   if( button.classList.toggle("ON")){
+    button.innerText="ON";
+   }else{
+    button.innerText="OFF";
+   }
+});
