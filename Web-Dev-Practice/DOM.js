5152c4b78dd219b0e1ad1bbd517f9d80e557ed1f
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-26 09:42:11 +0500

    add practice questions

--- Web-Dev-Practice/DOM.js
+++ Web-Dev-Practice/DOM.js
@@ -1,16 +1,16 @@
 // Project NO : 1 BUTTON CLICK ON OFF.
-let button=document.querySelector("button");
-button.addEventListener("click",function(){
-   if( button.classList.toggle("ON")){
-    button.innerText="ON";
-   }else{
-    button.innerText="OFF";
-   }
+let button = document.querySelector("button");
+button.addEventListener("click", function () {
+    if (button.classList.toggle("ON")) {
+        button.innerText = "ON";
+    } else {
+        button.innerText = "OFF";
+    }
 });
 
 
 // Project NO : 2 USE  OF INPUT EVENTLISNER.
-let input=document.querySelector("input");
-input.addEventListener("input",function(){
+let input = document.querySelector("input");
+input.addEventListener("input", function () {
     console.dir(input.value);
 });
