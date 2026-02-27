cd01f09e04dce41ae600c3e96392838e39133d64
Malik Waseem <malikwaseem123456564@gmail.com>
2026-02-27 14:29:17 +0500

    add questions

--- Web-Dev-Practice/DOM.js
+++ Web-Dev-Practice/DOM.js
@@ -9,14 +9,16 @@ button.addEventListener("click", function () {
 });
 
 
-// Project NO : 2 USE  OF INPUT EVENTLISNER.
+
+// Project NO : 2 USE  OF INPUT EVENTLISTENER.
 let input = document.querySelector("input");
 input.addEventListener("change", function (val) {
     console.log(val);
 });
 
 
-// Project NO : 3 USE  OF CHANGE EVENTLISNER.
+
+// Project NO : 3 USE  OF CHANGE EVENTLISTENER.
 let h3 = document.querySelector("h3");
 let select = document.querySelector("select");
 select.addEventListener("change", function (dets) {
@@ -24,8 +26,24 @@ select.addEventListener("change", function (dets) {
 });
 
 
-// Project NO : 4 USE  KEYDOWN EVENTLISNER.
+
+// Project NO : 4 USE  KEYDOWN EVENTLISTENER.
 let h1 = document.querySelector(" h1");
 window.addEventListener("keydown", function (val) {
     h1.textContent = `${val.key}`;
+});
+
+
+
+// Project NO : 5 USE  OF CLICK AND CHANGE EVENTLISTENER.
+let file=document.querySelector("#input");
+let btn=document.querySelector("#btn");
+btn.addEventListener("click",function(){
+file.click();
+});
+file.addEventListener("change",function(val){
+let file=val.target.files[0];
+if(file){
+    btn.textContent=file.name;
+}
 });
